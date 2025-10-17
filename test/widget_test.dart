92d27a4d28577af4d62215c03c7037be54659cc9
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-16 21:22:32 -0300

    test: creating client_model test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,84 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:app_med/main.dart';
+import 'package:test/test.dart';
+import 'package:intl/intl.dart';
+import 'package:app_med/models/client_model.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('ClientModel', () {
+    test('Deve criar um cliente corretamente', () {
+      final dataNascimento = DateTime(1990, 5, 20);
+      final client = ClientModel(
+        username: 'joao123',
+        dataDeNascimento: dataNascimento,
+        gender: 'Masculino',
+        senha: 'senha123',
+        cpf: '123.456.789-00',
+        email: 'joao@email.com',
+      );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      expect(client.username, 'joao123');
+      expect(client.dataDeNascimento, dataNascimento);
+      expect(client.gender, 'Masculino');
+      expect(client.senha, 'senha123');
+      expect(client.cpf, '123.456.789-00');
+      expect(client.email, 'joao@email.com');
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    test('dataFormatada() deve formatar a data corretamente', () {
+      final dataNascimento = DateTime(1990, 5, 20);
+      final client = ClientModel(
+        username: 'joao123',
+        dataDeNascimento: dataNascimento,
+        gender: 'Masculino',
+        senha: 'senha123',
+        cpf: '123.456.789-00',
+        email: 'joao@email.com',
+      );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      final dataFormatada = client.dataFormatada();
+      final expectedDataFormatada = DateFormat('yyyy-MM-dd').format(dataNascimento);
+
+      expect(dataFormatada, expectedDataFormatada);
+    });
+
+    test('toMap() deve retornar um mapa correto', () {
+      final dataNascimento = DateTime(1990, 5, 20);
+      final client = ClientModel(
+        username: 'joao123',
+        dataDeNascimento: dataNascimento,
+        gender: 'Masculino',
+        senha: 'senha123',
+        cpf: '123.456.789-00',
+        email: 'joao@email.com',
+      );
+
+      final map = client.toMap();
+
+      expect(map['username'], 'joao123');
+      expect(map['dataDeNascimento'], '1990-05-20');
+      expect(map['gender'], 'Masculino');
+      expect(map['senha'], 'senha123');
+      expect(map['cpf'], '123.456.789-00');
+      expect(map['email'], 'joao@email.com');
+    });
+
+    test('fromMap() deve criar um ClientModel correto', () {
+      final map = {
+        'username': 'joao123',
+        'dataDeNascimento': '1990-05-20',
+        'gender': 'Masculino',
+        'senha': 'senha123',
+        'cpf': '123.456.789-00',
+        'email': 'joao@email.com',
+      };
+
+      final client = ClientModel.fromMap(map);
+
+      expect(client.username, 'joao123');
+      expect(client.dataDeNascimento, DateTime(1990, 5, 20));
+      expect(client.gender, 'Masculino');
+      expect(client.senha, 'senha123');
+      expect(client.cpf, '123.456.789-00');
+      expect(client.email, 'joao@email.com');
+    });
   });
 }

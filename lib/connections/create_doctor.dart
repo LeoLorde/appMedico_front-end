ff116d574bf90bd3d3667cae0c4a621a94495025
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-18 16:32:03 -0300

    feat: all uuid in creation connections

--- lib/connections/create_doctor.dart
+++ lib/connections/create_doctor.dart
@@ -1,19 +1,24 @@
 import 'package:app_med/models/doctor_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:uuid/uuid.dart';
 
 Future<Map> createDoctor(DoctorModel doctorModel) async {
+  var uuid = Uuid();
+  String doctorId = uuid.v4();
+
   final response = await http.post(
     Uri.parse("http://192.168.1.10:5000/doctor/create"),
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({
+      "id": doctorId,
       "username": doctorModel.username,
       "senha": doctorModel.senha,
       "crm": doctorModel.crm,
       "especialidade": doctorModel.especialidade,
       "bio": doctorModel.bio,
       "email": doctorModel.email,
-      "endereco_id": 0,
+      "endereco_id": doctorModel.enderecoId,
     }),
   );
   if (response.statusCode != 201) {

09915c991dd3ffb5aad361c164e03d20050c5194
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-20 20:47:27 -0300

    feat: search doctor with debounce

--- lib/screens/search_doctor_screen.dart
+++ lib/screens/search_doctor_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:app_med/connections/search_doctor.dart';
 import 'package:app_med/models/doctor_model.dart';
 import 'package:app_med/widgets/doctor_search.dart';
@@ -11,8 +12,42 @@ class SearchDoctorScreen extends StatefulWidget {
 
 class _SearchDoctorScreenState extends State<SearchDoctorScreen> {
   List<DoctorModel> doctors = [];
-
   TextEditingController controller = TextEditingController();
+
+  Timer? _debounce;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onSearchChanged);
+    controller.dispose();
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () async {
+      final query = controller.text.trim();
+      if (query.isEmpty) {
+        setState(() => doctors = []);
+        return;
+      }
+
+      // Faz o fetch
+      List<DoctorModel> fetchedDoctors = await getDoctor(username: query);
+      print("Buscando por: $query | Encontrados: ${fetchedDoctors.length}");
+      setState(() {
+        doctors = fetchedDoctors;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,25 +59,13 @@ class _SearchDoctorScreenState extends State<SearchDoctorScreen> {
         controller: controller,
       ),
       body: ListView(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         children: [
-          ElevatedButton(
-            onPressed: () async {
-              List<DoctorModel> fetchedDoctors = await getDoctor(username: controller.text);
-              print("${controller.text}");
-              print(fetchedDoctors.length);
-              setState(() {
-                doctors = fetchedDoctors;
-              });
-            },
-            child: Text("FETCH"),
-          ),
-
           doctors.isEmpty
-              ? Center(child: Text('Nenhum doutor encontrado.'))
+              ? const Center(child: Text('Nenhum doutor encontrado.'))
               : ListView.builder(
                   shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
+                  physics: const NeverScrollableScrollPhysics(),
                   itemCount: doctors.length,
                   itemBuilder: (context, index) {
                     DoctorModel doctor = doctors[index];

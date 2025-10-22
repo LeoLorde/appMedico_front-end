9b82ce93e36f8635d58585bb3bba6bf240d11a30
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-22 07:49:16 -0300

    adding src/main/resource to .gitignore

--- src/main/java/FcmSender.java
+++ src/main/java/FcmSender.java
@@ -1,18 +1,20 @@
-private static void sendMessageToFcmRegistrationToken() throws Exception {
-   String registrationToken = "cvG7w-6oSe-G6ju53QlSUi:APA91bG2QHZbt6O4W08dg_Pxl7gGiyfq4Dd9Ac2MUbvXG2sNwJHc6WVteOV1wNouDxhLffcklKSRXhjAE-_bF83DhaC3D5VLw0bnSapXhxMTwiwV1WkO5mQ";
-   Message message =
-       Message.builder()
-           .putData("FCM", "https://firebase.google.com/docs/cloud-messaging")
-           .putData("flutter", "https://flutter.dev/")
-           .setNotification(
-               Notification.builder()
-                   .setTitle("Try this new app")
-                   .setBody("Learn how FCM works with Flutter")
-                   .build())
-           .setToken(registrationToken)
-           .build();
+public class FcmSender{  
+  private static void sendMessageToFcmRegistrationToken() throws Exception {
+    String registrationToken = "cvG7w-6oSe-G6ju53QlSUi:APA91bG2QHZbt6O4W08dg_Pxl7gGiyfq4Dd9Ac2MUbvXG2sNwJHc6WVteOV1wNouDxhLffcklKSRXhjAE-_bF83DhaC3D5VLw0bnSapXhxMTwiwV1WkO5mQ";
+    Message message =
+        Message.builder()
+            .putData("FCM", "https://firebase.google.com/docs/cloud-messaging")
+            .putData("flutter", "https://flutter.dev/")
+            .setNotification(
+                Notification.builder()
+                    .setTitle("Try this new app")
+                    .setBody("Learn how FCM works with Flutter")
+                    .build())
+            .setToken(registrationToken)
+            .build();
 
-   FirebaseMessaging.getInstance().send(message);
+    FirebaseMessaging.getInstance().send(message);
 
-   System.out.println("Message to FCM Registration Token sent successfully!!");
- }
+    System.out.println("Message to FCM Registration Token sent successfully!!");
+  }
+}
